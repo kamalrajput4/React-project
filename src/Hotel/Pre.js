3d8ee9db18188d02aa0842de4e3e4057f86a42bc
kamalrajput4 <kmlrohilla531@gmail.com>
2025-12-09 13:01:34 +0530

    added pre tage one more

--- src/Hotel/Pre.js
+++ src/Hotel/Pre.js
@@ -7,6 +7,11 @@ const Pre = () => {
         deserve the chance on their own hotel adventure,discovering<br></br>
         comfort and styles admist new surroundings.
       </pre>
+
+       <pre> We embrance the allure of wanaderlust , recognizing that evrything <br></br>
+        deserve the chance on their own hotel adventure,discovering<br></br>
+        comfort and styles admist new surroundings.
+      </pre>git
     </div>
   )
 }
